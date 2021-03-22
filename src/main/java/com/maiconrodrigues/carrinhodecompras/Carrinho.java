dcf565616ece18ce24039bc88ad49904f3295e97
Maicon Rodrigues <nociamrq27@gmail.com>
2021-03-21 23:39:20 -0300

    projeto em andamento

---
+++ src/main/java/com/maiconrodrigues/carrinhodecompras/Carrinho.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.maiconrodrigues.carrinhodecompras;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Maicon
+ */
+public class Carrinho {
+    private String nome;
+    private String quantiade;
+     private String preco;
+    
+  
+
+    public Carrinho() {
+    }
+
+    public Carrinho(String nome, String quantiade, String preco) {
+        this.nome = nome;
+        this.quantiade = quantiade;
+        this.preco = preco;
+    }
+
+ 
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the quantiade
+     */
+    public String getQuantiade() {
+        return quantiade;
+    }
+
+    /**
+     * @param quantiade the quantiade to set
+     */
+    public void setQuantiade(String quantiade) {
+        this.quantiade = quantiade;
+    }
+
+    /**
+     * @return the preco
+     */
+    public String getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    /**
+     * @return the ListaDeCompras
+     */
+   
+
+    /**
+     * @param ListaDeCompras the ListaDeCompras to set
+     */
+    /*public void setListaDeCompras(ArrayList<Produto> ListaDeCompras) {
+        this.ListaDeCompras = ListaDeCompras;
+    }*/
+    
+    
+    
+
+    /**
+     *
+     * @param nome
+     * @param quantiade
+     * @param preco
+     * @param ListaDeCompras
+     */
+    
+   
+   
+           
+
+    
+    
+}
